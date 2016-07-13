cc7551f62924b273eade351905b323b76d3b3191
Nilesh_ordex <nilesh.kasvala@ordextechnology.com>
2016-07-13 11:37:50 +0530

    Update INTULocationManager.m
    - (void)processRecurringRequest:(INTULocationRequest *)locationRequest
    This method use and crash the app

--- LocationManager/INTULocationManager/INTULocationManager.m
+++ LocationManager/INTULocationManager/INTULocationManager.m
@@ -3,7 +3,7 @@
 //
 //  Copyright (c) 2014-2015 Intuit Inc.
 //
-//  Permission is hereby granted, free of charge, to any person obtaining
+//  Permission is hereby granted, free of charge, to any person obtainingdf
 //  a copy of this software and associated documentation files (the
 //  "Software"), to deal in the Software without restriction, including
 //  without limitation the rights to use, copy, modify, merge, publish,
